8bd186476da6ca35cb5ab7fab03ac5a944f5574c
F-Jan <87376666+F-Jan@users.noreply.github.com>
2022-02-15 20:12:49 +0100

    Removed CustomGuiObject Class and merged it into the GuiObject Class

--- src/main/java/com/github/papermcplugin_guiapi/gui/object/GuiObject.java
+++ src/main/java/com/github/papermcplugin_guiapi/gui/object/GuiObject.java
@@ -17,10 +17,20 @@ public class GuiObject {
 
     private final List<GuiObjectClickEvent> clickEventList = new ArrayList<>();
 
+    private final ItemStack itemStack;
+
+    public GuiObject(ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public GuiObject() {
+        this.itemStack = new ItemStack(Material.BARRIER);
+    }
+
     /**
      * Add an Event which gets triggered when the Gui is Clicked
      *
-     * @param event
+     * @param event The event to add
      */
     public void addClickEvent(GuiObjectClickEvent event) {
         clickEventList.add(event);
@@ -29,7 +39,7 @@ public class GuiObject {
     /**
      * Call to trigger a click Event
      *
-     * @param player
+     * @param player The Player who clicked the inventory
      */
     public void callClickEvents(Player player) {
         for (GuiObjectClickEvent event : clickEventList) {
@@ -38,11 +48,11 @@ public class GuiObject {
     }
 
     /**
-     * Get the ItemStack in the InventoryGui
      *
-     * @return
+     * @return The ItemStack of the GuiObject
      */
     public ItemStack getItemStack() {
-        return new ItemStack(Material.BARRIER);
+        return itemStack;
     }
+
 }
